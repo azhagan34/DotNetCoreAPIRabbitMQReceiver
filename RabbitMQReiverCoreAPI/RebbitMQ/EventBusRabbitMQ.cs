d8e381e38c9de1118a9bfb9f8a0f9f4c42f348db
Vivekanandam <AVIVEKA1@ford.com>
2020-09-07 13:01:11 +0530

    Initial commit

---
+++ RabbitMQReiverCoreAPI/RebbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQReiverCoreAPI.Models;
+using RabbitMQReiverCoreAPI.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQReiverCoreAPI.RebbitMQ
+{
+    public class EventBusRabbitMQ : IDisposable
+    {
+        private readonly IRabbitMQPersistentConnection _persistentConnection;
+        private IModel _consumerChannel;
+        private string _queueName;
+        UserService _userService = new UserService();
+
+        public EventBusRabbitMQ(IRabbitMQPersistentConnection persistentConnection, string queueName = null)
+        {
+            _persistentConnection = persistentConnection ?? throw new ArgumentNullException(nameof(persistentConnection));
+            _queueName = queueName;
+        }
+
+        public IModel CreateConsumerChannel()
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            var channel = _persistentConnection.CreateModel();
+            //channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.ExchangeDeclare("pipes_direct", type: "direct");
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                           exchange: "pipes_direct",
+                           routingKey: "both");
+            var consumer = new EventingBasicConsumer(channel);
+
+            //consumer.Received += async (model, e) =>
+            //{
+            //    var eventName = e.RoutingKey;
+            //    var message = Encoding.UTF8.GetString(e.Body);
+            //    channel.BasicAck(e.DeliveryTag, multiple: false);
+            //};
+
+            //Create event when something receive
+            consumer.Received += ReceivedEvent;
+
+
+
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            channel.CallbackException += (sender, ea) =>
+            {
+                _consumerChannel.Dispose();
+                _consumerChannel = CreateConsumerChannel();
+            };
+            return channel;
+        }
+
+        private void ReceivedEvent(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            List<RequestQ> lstrequ = new List<RequestQ>();
+            lstrequ = JsonConvert.DeserializeObject<List<RequestQ>>(message);
+            //JsonSerializer.Deserialize<List<RequestQ>>();
+            Console.WriteLine(" [x] Received {0}", message);
+        }
+
+        public void PublishUserSaveFeedback(string _queueName, UserSaveFeedback publishModel, IDictionary<string, object> headers)
+        {           
+
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            using (var channel = _persistentConnection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+                properties.Headers = headers;
+                // properties.Expiration = "36000000";
+                //properties.ContentType = "text/plain";
+
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: "", routingKey: _queueName, mandatory: true, basicProperties: properties, body: body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Sent RabbitMQ");
+                    //implement ack handle
+                };
+                channel.ConfirmSelect();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_consumerChannel != null)
+            {
+                _consumerChannel.Dispose();
+            }
+        }
+    }
+    public class RequestQ
+    {
+        public int REQUEST_NUM;
+
+        public bool Received_by_RefreshWS;
+
+        public bool Received_by_Mobility_BackEnd;
+    }
+}
